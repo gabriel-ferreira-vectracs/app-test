2a696efbb5b754d7ce910c56a9f28a4f2031185c
Gabriel Carvalho Ferreira <gabriel.ferreira@vectracs.com.br>
2023-01-27 16:36:42 -0300

    Implementação da página simples para testar deploy na Azure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-test';
+
+  ngOnInit(): void {
+    this.startAnimation();
+  }
+
+  startAnimation(): void {
+    const classes = ['v', 'e', 'c', 't', 'r', 'a']
+    let count = 0;
+    let intervalMiliseconds = 100;
+
+    const setTimer = setInterval(() => {
+    const letter = <HTMLElement>document.querySelector(`.${classes[count]}`);
+    letter.classList.add('anime-letter')
+    console.log(classes[count])
+    intervalMiliseconds += 100;
+    
+    if (count == classes.length - 1) {
+        console.log('sim')
+        clearInterval(setTimer)
+    }
+    count++;
+}, intervalMiliseconds)
+  }
+
 }
